655ae9d01cebecdb180eafaaaf0635d0abaa0b0e
Herman Bimikwe <bimikwe.herman@gmail.com>
2021-08-02 11:14:04 -0500

    Exercise 1 complete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,14 +1,14 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-//        System.out.println("Hello World");
-        System.out.print("Hello World");
-        System.out.print("\n");
-        System.out.print("Hello World");
+//public class HelloWorld {
+//    public static void main(String[] args) {
+////        System.out.println("Hello World");
+//        System.out.print("Hello World");
 //        System.out.print("\n");
-        "javac HelloWorld.java"
-
-
-        int myFavoriteNumber;
-    }
-
-}
+//        System.out.print("Hello World");
+////        System.out.print("\n");
+//        "javac HelloWorld.java"
+//
+//
+//        int myFavoriteNumber;
+//    }
+//
+//}

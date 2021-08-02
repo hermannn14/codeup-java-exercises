09870fd8b5a595d73e6f3026cde7f3405255c99a
Herman Bimikwe <bimikwe.herman@gmail.com>
2021-08-02 16:40:47 -0500

    Do while loop complete

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,10 +1,31 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        int i = 5;
-        while (i <= 15) {
-            System.out.println( i);
-            i++;
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println( i);
+//            i++;
+//        }
+//
+//        int i = 2;
+//        do {
+//            System.out.println("i");
+//        } while (i <= 100);
+
+//        int i = 100;
+//        do {
+//            System.out.println(i);
+//            i = i - 5;
+//        }
+//        while (i >= -10);
+
+
+        long i = 2;
+        do {
+            System.out.println(i);
+            i = (long) Math.pow(i, 2);
         }
+        while (i <= 1000000);
+
 
     }
 }

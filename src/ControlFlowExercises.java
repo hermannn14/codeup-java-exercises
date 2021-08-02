35f5260c9f6fd8a48fcc90d29c315d5c5c4cd357
Herman Bimikwe <bimikwe.herman@gmail.com>
2021-08-02 17:04:20 -0500

    loops Exercise 1 completed

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -19,13 +19,27 @@ public class ControlFlowExercises {
 //        while (i >= -10);
 
 
-        long i = 2;
-        do {
-            System.out.println(i);
-            i = (long) Math.pow(i, 2);
-        }
-        while (i <= 1000000);
+//        long i = 2;
+//        do {
+//            System.out.println(i);
+//            i = (long) Math.pow(i, 2);
+//        }
+//        while (i <= 1000000);
 
+//        for(int i = 0; i <= 15 ; i ++) {
+//            System.out.println(i);
+//        }
+//        for (int i = 0; i<= 100 ; i+= 2) {
+//            System.out.println(i);
+//        }
+
+//        for (int i = 100; i >= 0 ; i-= 5) {
+//            System.out.println(i);
+//        }
+
+        for (long i = 2; i < 1000000 ; i= (long) Math.pow(i,2)) {
+            System.out.println(i);
+        }
 
     }
 }

dc75568f609675dbb3bb2d05891e32cc6fb22bfb
Herman Bimikwe <bimikwe.herman@gmail.com>
2021-08-03 12:01:40 -0500

    Java strings exercises completed

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -1,12 +1,20 @@
-public class StringExercise {
-    public static void main(String[] args) {
-        String mamaMia  = "I have never seen this movie";
-        System.out.printf(mamaMia);
-        System.out.println();
-        String check = "Check \"this\" out!, \"s inside of \s!";
-        System.out.println(check);
-
-
-
-    }
-}
+//public class StringExercise {
+//    public static void main(String[] args) {
+//        String mamaMia  = "We don't need no education\n" +
+//                "We don't need no thought control";
+//        System.out.printf(mamaMia);
+//        System.out.println();
+//        String check = "Check \"this\" out!, \"s inside of \"s!";
+//        System.out.println(check);
+//        System.out.println();
+//        String windows = "In windows, the main drive is usually C:\\";
+//        System.out.println(windows);
+//        String backlashes = "I can do backslashes \\, double backslashes \\\\,\n" +
+//                "and the amazing triple backslash \\\\\\!";
+//        System.out.println(backlashes);
+//
+//
+//
+//
+//    }
+//}
---
+++ src/bob.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class bob {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hey Bob want to go play soccer?");
+        String bobResponse = scanner.nextLine();
+        if (bobResponse.endsWith(".")) {
+            System.out.println("Stop being a lazy bum");
+        } else if (bobResponse.endsWith("!")) {
+            System.out.println("Bro, c'mon");
+        } else if (bobResponse.isEmpty()) {
+            System.out.println("Fine. Be that way!");
+        } else {
+            System.out.println("Absolutely");
+        }
+
+    }
+}

eb6b032be0234b83904969344a177b8fd66db520
Herman Bimikwe <bimikwe.herman@gmail.com>
2021-08-06 11:43:45 -0500

    Finished exercise 3 Obj Oriented

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,9 +1,17 @@
-//package shapes;
-//
-//public class Circle {
-//    public Circle(double radius);
-//
-//    public double getArea();
-//
-//    public double getCircumference();
-//}
+package shapes;
+
+public class Circle {
+    private double radius;
+
+    public Circle (double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+}

309cf0d34b556ef2c82b716d9235581330d3310a
Herman Bimikwe <bimikwe.herman@gmail.com>
2021-08-05 19:30:49 -0500

    Stuck on exercise 1b Obj orienting

--- src/Person.java
+++ src/Person.java
@@ -26,5 +26,22 @@ public class Person {
         person.sayHello();
         person.setName("Mike");
         System.out.println(person.getName());
+
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
+//        person2.setName("Jane");
+//        System.out.println(person1.getName());
+//        System.out.println(person2.getName());
     }
 }
---
+++ src/util/Input.java
@@ -0,0 +1,32 @@
+package util;
+
+public class Input {
+    private String scanner;
+
+    private String getString() {
+        return null;
+    }
+
+    private boolean yesNo() {
+        return true;
+    }
+
+    private int getInt(int min, int max) {
+        return (max);
+    }
+
+    private int getInt() {
+        return 6;
+    }
+
+    private double getDouble(double min, double max) {
+        return min;
+    }
+
+    private double getDouble() {
+        return 5;
+    }
+
+
+
+}

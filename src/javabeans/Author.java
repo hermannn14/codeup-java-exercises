d4f5e97e5c6d2752265b77b8450a236177d374f8
Herman Bimikwe <bimikwe.herman@gmail.com>
2021-09-06 17:22:32 -0500

    javabeans exercise 2 complete

--- src/javabeans/Author.java
+++ src/javabeans/Author.java
@@ -1,6 +1,17 @@
 package javabeans;
 
-public class Author {
+import java.io.Serializable;
+
+public class Author implements Serializable {
+    private int id;
+    private String firstName;
+    private String lastName;
     public Author() {
+    }
+
+    public Author(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 }
